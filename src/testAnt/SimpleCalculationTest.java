bbd27ab75030d5e9fa54f36836309ab16055d020
sl_wang <fenhongtaohua@gmail.com>
2015-09-11 14:58:25 +0800

    second commit

--- src/testAnt/SimpleCalculationTest.java
+++ src/testAnt/SimpleCalculationTest.java
@@ -13,5 +13,11 @@ public class SimpleCalculationTest {
 		int c=sc.Add(1, 2);
 		assertEquals(3,c);
 	}
+	
+	@Test
+	public void testTimes(){
+		int a=sc.times(2, 3);
+		assertEquals(5,a);
+	}
 
 }
--- src/testAnt/SimpleCalulation.java
+++ src/testAnt/SimpleCalulation.java
@@ -4,4 +4,8 @@ public class SimpleCalulation {
 	public int Add(int a,int b){
 		return (a+b);
 	}
+	
+	public int times(int a,int b){
+		return (a*b);
+	}
 }
